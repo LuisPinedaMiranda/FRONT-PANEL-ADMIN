b190dc3e46ea673f53872d6a7f3c5e1f02ea1c93
LuisPinedaMiranda <luis.pineda@cyberpeace.tech>
2024-11-04 12:42:07 -0600

    Login View and Sidebar and TopNav components

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import Sidebar from './components/Sidebar.vue'
+
+
+export default{
+  data() {
+    return {
+      
+    }
+  },
+  components:{
+    Sidebar,
+  
+  }
+}
+
+</script>
+
+<template>
+ <!--    <Sidebar/> -->
+  <RouterView/>
+</template>
+
+<style scoped>
+a{
+  color: #6e84a3 !important;
+}
+</style>
